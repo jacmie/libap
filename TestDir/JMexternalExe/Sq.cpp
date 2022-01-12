243ade2d920645d3ec3ebc48e20a6fd0b7270b31
JM <jmieloszyk@meil.pw.edu.pl>
2022-01-12 14:47:10 +0100

    Sq Output to cout

--- TestDir/JMexternalExe/Sq.cpp
+++ TestDir/JMexternalExe/Sq.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main(int argc, char *argv[]) 
 {
-    clog << endl << "=== Square.exe ===" << endl << endl;
+    cout << endl << "=== Square.exe ===" << endl << endl;
 
-    clog << "ArgNr: " << argc << endl << endl;
+    cout << "ArgNr: " << argc << endl << endl;
 
     double Sum = 1;
 
     for(int a=1; a<argc; a++)
     {
         Sum *= strtod(argv[a], NULL);
-        clog << "Arg_" << a << "\t" << argv[a] << "\t" << Sum << endl;
+        cout << "Arg_" << a << "\t" << argv[a] << "\t" << Sum << endl;
     }
     
-    clog << endl << "Sum = " << Sum << endl << endl;
+    cout << endl << "Sum = " << Sum << endl << endl;
 
     return 0;
 }
