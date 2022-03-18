3d0596d0f6acb75a2a244dfb38667ef301acd242
JM <jmieloszyk@meil.pw.edu.pl>
2022-03-18 15:46:33 +0100

    JMdialogs resize improved

--- CommonTest.cpp
+++ CommonTest.cpp
@@ -88,7 +88,14 @@ int main(int argc, char *argv[])
 
 	clog << endl << "*** JMdialogs ***" << endl << endl;
 
-    JM_message("Hello World!!!");
+	fl_message_font(FL_HELVETICA, 12);
+
+    JM_alert("Alert!!!");
+    JM_message("Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3!!!");
+    JM_choice("Asce3e3e  e3e3e3e 3e 33e 3e3e3er2r2ttt 222       2r2r2r2rr2r2r  2r2r 2r2tt2t2?1?", "Yes", "No", "Cancel");
+    JM_input("Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3Hello World e 3e 3e3eeeeeeeeeeeeee3e3e3 3e3 e3e3 e3e3e3e3eeee 3e3e3e3e  3e3e3e3e3e3e 3e3eee3Input Hello Worldr  rr22r2r2r 2r2r 22r 2 r2r 2r2r 2r222r 22ty2t2t 2r r2r2r 22r2r 2 r2rr  2r22!!!");
+    JM_password("Hello World!!!");
+
     exit(1);
 
 	// *** JMconvert ***
--- JMdialogs.h
+++ JMdialogs.h
@@ -1,6 +1,8 @@
 #ifndef DIALOGS_H
 #define DIALOGS_H
 
+// Implementation of fl_message, fl_ask, fl_choice, fl_input etc.
+
 #include <FL/fl_ask.H>
 
 /*
@@ -13,34 +15,18 @@
 #  endif // __GNUC__
 */
 
+FL_EXPORT void JM_alert(const char *,...) __fl_attr((__format__ (__printf__, 1, 2)));
 FL_EXPORT void JM_message(const char *,...) __fl_attr((__format__ (__printf__, 1, 2)));
-
-//void JM_message(const char *,...);
-
-/*FL_EXPORT void fl_alert(const char *,...) __fl_attr((__format__ (__printf__, 1, 2)));
-// fl_ask() is deprecated since it uses "Yes" and "No" for the buttons,
-// which does not conform to the current FLTK Human Interface Guidelines.
-// Use fl_choice() instead with the appropriate verbs instead.
-FL_EXPORT int fl_ask(const char *,...) __fl_attr((__format__ (__printf__, 1, 2), __deprecated__));
-FL_EXPORT int fl_choice(const char *q,const char *b0,const char *b1,const char *b2,...) __fl_attr((__format__ (__printf__, 1, 5)));
-FL_EXPORT const char *fl_input(const char *label, const char *deflt = 0, ...) __fl_attr((__format__ (__printf__, 1, 3)));
-FL_EXPORT const char *fl_password(const char *label, const char *deflt = 0, ...) __fl_attr((__format__ (__printf__, 1, 3)));
-
-// since FLTK 1.3.8:
-FL_EXPORT int fl_choice_n(const char *q,const char *b0,const char *b1,const char *b2,...) __fl_attr((__format__ (__printf__, 1, 5)));
-
-FL_EXPORT Fl_Widget *fl_message_icon();
+FL_EXPORT int JM_choice(const char *q,const char *b0,const char *b1,const char *b2,...) __fl_attr((__format__ (__printf__, 1, 5)));
+FL_EXPORT const char *JM_input(const char *label, const char *deflt = 0, ...) __fl_attr((__format__ (__printf__, 1, 3)));
+FL_EXPORT const char *JM_password(const char *label, const char *deflt = 0, ...) __fl_attr((__format__ (__printf__, 1, 3)));
+FL_EXPORT int JM_choice_n(const char *q,const char *b0,const char *b1,const char *b2,...) __fl_attr((__format__ (__printf__, 1, 5)));
+/*
 extern FL_EXPORT Fl_Font fl_message_font_;
 extern FL_EXPORT Fl_Fontsize fl_message_size_;
-inline void fl_message_font(Fl_Font f, Fl_Fontsize s) {
-  fl_message_font_ = f; fl_message_size_ = s;}
-
-FL_EXPORT void fl_message_hotspot(int enable);
-FL_EXPORT int fl_message_hotspot(void);
-
-FL_EXPORT void fl_message_title(const char *title);
-FL_EXPORT void fl_message_title_default(const char *title);
-
+inline void fl_message_font(Fl_Font f, Fl_Fontsize s) {fl_message_font_ = f; fl_message_size_ = s;}
+*/
+/*
 // pointers you can use to change FLTK to a foreign language:
 extern FL_EXPORT const char* fl_no;
 extern FL_EXPORT const char* fl_yes;
