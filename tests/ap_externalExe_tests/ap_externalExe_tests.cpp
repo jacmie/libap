87bef4f86a6d6659984b3dff469e7e7f02cd2db6
jacmie <mieloszyk.jacek@gmail.com>
2023-05-02 19:35:35 +0200

    VS specific code

--- tests/ap_externalExe_tests/ap_externalExe_tests.cpp
+++ tests/ap_externalExe_tests/ap_externalExe_tests.cpp
@@ -33,7 +33,7 @@ TEST(ap_externalExe_tests, execute_binary) {
 	//D:\a\libap\libap\extern\googletest\googletest\src\gtest_main.cc
     //arg.push_back("../../../../../build/tests/sq"); 
     //arg.push_back("../../build/tests/sq.exe"); 
-    arg.push_back("D:/a/libap/libap/build/tests/sq.exe"); 
+    arg.push_back("D:/a/libap/libap/build/tests/Release/sq.exe"); 
     //arg.push_back("C:/msys64/usr/local/bin/sq.exe");  	// Global directory
 #else
     arg.push_back("../../build/tests/sq"); 
