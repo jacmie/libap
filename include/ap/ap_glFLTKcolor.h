dc61199a80c236107443ea08b9ef2a6d160f6b33
jacmie <mieloszyk.jacek@gmail.com>
2023-05-04 19:06:23 +0200

    fltk_jpeg for macos

--- include/ap/ap_glFLTKcolor.h
+++ include/ap/ap_glFLTKcolor.h
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #if defined(__APPLE__)
+	#define GL_SILENCE_DEPRECATION
 	#include <OpenGL/gl.h>
 #else
 	#include <GL/gl.h>
--- include/ap/ap_glTechFont.h
+++ include/ap/ap_glTechFont.h
@@ -6,6 +6,7 @@
 #endif
 
 #if defined(__APPLE__)
+	#define GL_SILENCE_DEPRECATION
 	#include <OpenGL/gl.h>
 #else
 	#include <GL/gl.h>
